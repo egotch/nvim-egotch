522aa0404d82097021b1ffbaa090ae3df5e90ef3
Eric Gotkowski <egotkowski@gmail.com>
2024-04-21 14:22:20 -0500

    adding vim tab navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -16,3 +16,15 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+
+-- Tab switching easier
+vim.keymap.set('n', '<leader>t', 'gt')
+vim.keymap.set('n', '<leader>1', '1gt')
+vim.keymap.set('n', '<leader>2', '2gt')
+vim.keymap.set('n', '<leader>3', '3gt')
+vim.keymap.set('n', '<leader>4', '4gt')
+vim.keymap.set('n', '<leader>5', '5gt')
+vim.keymap.set('n', '<leader>6', '6gt')
+vim.keymap.set('n', '<leader>7', '7gt')
+vim.keymap.set('n', '<leader>8', '8gt')
+vim.keymap.set('n', '<leader>9', '9gt')
