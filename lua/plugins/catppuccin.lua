42c77ce84beffabe73b775496efece4a82825d45
Eric Gotkowski <egotkowski@gmail.com>
2024-04-21 13:38:11 -0500

    adding init.lua and all plugins * themes * git-stuff (git-signs, vim-fugitive) * lsp * lualine * neo-tree * none-ls * telescope * treesitter

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    --config = function()
+    -- vim.cmd.colorscheme "catppuccin-mocha"
+    --end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {}
+  },
+
+}
