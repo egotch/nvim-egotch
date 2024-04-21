b977cc91f6fde2777aafb0d83c1099f33fac1b0d
Eric Gotkowski <egotkowski@gmail.com>
2024-04-21 13:54:31 -0500

    Adding black and prettier auto-formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,12 +6,11 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.erb_lint,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
       },
     })
-
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("v", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
