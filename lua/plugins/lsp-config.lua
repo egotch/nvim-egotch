3d1bd05cd5237fb23e739891ad699cd19aae3def
Eric Gotkowski <egotkowski@gmail.com>
2024-04-21 21:15:34 -0500

    adding todo highlighting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,8 +25,8 @@ return {
       lspconfig.pyright.setup({})
 
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<c-q>', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<c-b>', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end
