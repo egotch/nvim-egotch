7ebb8343a56ef55422d27dbf7058d344aa2a98df
Eric Gotkowski <eric.gotkowski@q2.com>
2024-04-22 09:06:04 -0500

    adding python debugger  Please enter the commit message for your changes. Lines starting

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,32 +1,34 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "mfussenegger/nvim-dap-python",
+    "leoluz/nvim-dap-go",
+    "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
-	},
-	config = function()
+  },
+  config = function()
     require("dapui").setup()
-		require("dap-go").setup()
+    require("dap-go").setup()
+    require("dap-python").setup()
 
-		local dap, dapui = require("dap"), require("dapui")
+    local dap, dapui = require("dap"), require("dapui")
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-	end,
+    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
 }
