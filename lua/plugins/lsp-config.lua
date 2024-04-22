bf59d4add21943832758463a60ad708d29dc2d45
Eric Gotkowski <egotkowski@gmail.com>
2024-04-21 22:23:11 -0500

    adding pylsp for definition hovering

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "gopls", "pyright" }
+        ensure_installed = { "lua_ls", "tsserver", "gopls", "pyright" , "pylsp" }
       })
     end
   },
@@ -23,6 +23,7 @@ return {
       lspconfig.lua_ls.setup({})
       lspconfig.gopls.setup({})
       lspconfig.pyright.setup({})
+      lspconfig.pylsp.setup({})
 
 
       vim.keymap.set('n', '<c-q>', vim.lsp.buf.hover, {})
