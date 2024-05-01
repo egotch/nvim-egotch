b2ee6df398e884d9d8ee294c7f0e4d050046abf6
Eric Gotkowski <eric.gotkowski@q2.com>
2024-04-30 22:50:40 -0500

    updating file name to relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,11 +4,15 @@ return {
     require("lualine").setup({
       options = {
         theme = "catppuccin-mocha",
-        sections = {
-          lualine_a = {
-            file = 1,
-          },
-        },
+
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {{'filename', path=1}},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
       },
     })
   end,
