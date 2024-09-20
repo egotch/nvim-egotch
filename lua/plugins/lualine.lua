12d96497605872347109450c2709cffd06a5403f
Eric Gotkowski <eric.gotkowski@q2.com>
2024-09-20 13:27:33 -0500

    updating lualine
    * adding full file path in pos C
     * updating pos A to include mode + curr file name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,9 +7,9 @@ return {
 
       },
       sections = {
-        lualine_a = {'mode'},
+        lualine_a = {'mode', {'filename', path=0}},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {{'filename', path=1}},
+        lualine_c = {{'filename', path=3}},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
